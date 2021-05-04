3a841803dbe79292bc75b8804f232f8d92ff7a14
Pooya Golchian <pooya.golchian@gmail.com>
2021-05-04 20:50:00 +0430

    reformat

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,10 @@
 export const companies = [
-  {
-    id: 1,
-    value: "Viljatootja AS",
-  },
-  {
-    id: 2,
-    value: "Tartu Agro OÜ",
-  },
+    {
+        id: 1,
+        value: 'Viljatootja AS',
+    },
+    {
+        id: 2,
+        value: 'Tartu Agro OÜ',
+    },
 ]
